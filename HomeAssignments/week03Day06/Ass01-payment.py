e96afa45b6349be947ce89eeebe8b7bf5f008189
asksakthe <asksakthe@outlook.com>
2025-09-16 11:01:37 +0530

    practise Home Ass week04 sync 02

--- HomeAssignments/week03Day06/Ass01-payment.py
+++ HomeAssignments/week03Day06/Ass01-payment.py
@@ -1,26 +1,36 @@
 #payment system - polymorphism
 
 class CreditCardPayment:
-    def process_payment(amount):
-        return f"Processing credit card payment of {amount}"
+    def process_payment(self,amount):
+        self.amount = amount
+        return f"Processing credit card payment of {self.amount}"
     
 
 class  PayPalPayment:
-    def process_payment(amount):
-        return f"Processing PayPal payment of {amount}"
+    def process_payment(self,amount):
+        self.amount = amount
+        return f"Processing PayPal payment of {self.amount}"
     
 
 class  BankTransferPayment:
-    def process_payment(amount):
-        return f"Processing bank transfer of {amount}"
+    def process_payment(self,amount):
+        self.amount = amount
+        return f"Processing bank transfer of {self.amount}"
     
 
+# ui = BankTransferPayment()
+# print(ui.process_payment(900))
 def make_payment(payment_method, amount):
-    print(payment_method(amount))
+    transac = payment_method()
+    return transac.process_payment(amount)
+    #print(payment_method(amount))
 
 
 if __name__ == "__main__":
-    payMethods = ["PayPalPayment","BankTransferPayment","CreditCardPayment"]
+    payMethods = [PayPalPayment,BankTransferPayment,CreditCardPayment]
     for i in payMethods:
-        transac = i()
-        i.acc
+        print(i.__name__)
+        call_func = make_payment(i, 500)
+        print(call_func)
+
+        
